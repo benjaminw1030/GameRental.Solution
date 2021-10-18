66a536b1f9a2d5da4bf5619d217654c37ecad3e1
Ben Wilson <benjaminw1030@gmail.com>
2021-10-18 13:58:42 -0700

    Add games and home controller code
    Co-authored-by: Scott Hutley <scotthutley1@comcast.net>

---
+++ GameRental/Controllers/GamesController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using GameRental.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace GameRental.Controllers
+{
+  [Authorize]
+  public class GamesController : Controllers
+  {
+    private readonly GameRentalContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public GamesController(UserManager<ApplicationUser> userManager, GameRentalContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userGames = _db.Games.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userGames);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.DeveloperId = new SelectList(_db.Developers, "DeveloperId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Game game, int DeveloperId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      game.User = currentUser;
+      _db.Games.Add(game);
+      _db.SaveChanges();
+      if (DeveloperId != 0)
+      {
+        _db.DeveloperGame.Add(new DeveloperGame() { DeveloperId = DeveloperId, GameId = game.GameId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisGame = _db.Games
+        .Include(game => game.JoinEntities)
+        .ThenInclude(join => join.Developer)
+        .FirstOrDefault(game => game.GameId == id);
+      return View(thisGame);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisGame = _db.Games.FirstOrDefault(game => game.GameId == id);
+      ViewBag.DeveloperId = new SelectList(_db.Developers, "DeveloperId", "Name");
+      return View(thisGame);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Game game, int DeveloperId)
+    {
+      if (DeveloperId != 0)
+      {
+        _db.DeveloperGame.Add(new DeveloperGame() { DeveloperId = DeveloperId, GameId = game.GameId});
+      }
+      _db.Entry(game).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisGame = _db.Games.FirstOrDefault(game => game.GameId == id);
+      return View(thisGame);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisGame = _db.Games.FirstOrDefault(game => game.GameId == id);
+      _db.Games.Remove(thisGame);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    
+    public ActionResult AddDeveloper(int id)
+    {
+      var thisGame = _db.Games.FirstOrDefault(game => game.GameId == id);
+      ViewBag.DeveloperId = new SelectList(_db.Developers, "DeveloperId", "Name");
+      return View(thisGame);
+    }
+
+    [HttpPost]
+    public ActionResult AddDeveloper(Game game, int DeveloperId)
+    {
+      if (DeveloperId != 0)
+      {
+        _db.DeveloperGame.Add(new DeveloperGame() { DeveloperId = DeveloperId, GameId = game.GameId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteDeveloper(int joinId)
+    {
+      var joinEntry = _db.DeveloperGame.FirstOrDefault(entry => entry.DeveloperGameId == joinId);
+      _db.DeveloperGame.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
